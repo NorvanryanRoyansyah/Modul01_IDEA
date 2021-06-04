377084424a5b37545bbcb2d48ee469d13b62740c
Norvanryan Royansyah <ryanjaa9@gmail.com>
2021-06-04 23:05:36 +0800

    Membuat Class Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Norvan";
+        String namaTengah = "Ryan";
+        String namaBelakang = "Royansyah";
+        int usia = 19;
+        int targetTahunKuliah = 4;
+        double ipk = 3.900;
+        char nilaiAbjad = 'A';
+
+        System.out.println(namaDepan);
+    }
+}
