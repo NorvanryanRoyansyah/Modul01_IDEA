6d15fbd0417e585212a21ce014f5e84b350c6743
Norvanryan Royansyah <ryanjaa9@gmail.com>
2021-06-05 01:55:50 +0800

    Operasi String

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,22 @@ public class Main {
         char nilaiAbjad = 'A';
 
         System.out.println(namaDepan);
+
+        char[] uniskaChar = {'U','N','I','S','K','A',};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan +" "+ namaTengah +" "+ namaBelakang;
+        System.out.println(namaLengkap);
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(uniskaString.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(8));
+        System.out.println(namaLengkap.substring(8,12));
+
+        String[] namaArray = namaLengkap.split    (" ");
+
+        for (String nama : namaArray)
+            System.out.println(nama);
     }
 }
